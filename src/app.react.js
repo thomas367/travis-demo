d36f563ab29e9e668632cce25a8ce1470ac374c8
thomas367 <thomas.367@hotmail.com>
2019-10-09 20:45:20 +0300

    setup basic react

---
+++ src/app.react.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './app.scss';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div className='app'>
+                <p>Hello</p>
+            </div>
+        );
+    }
+}
+
+export default App;
